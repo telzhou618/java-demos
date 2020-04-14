4e617c84011fb3cb61cc1855c7c1ec06b1861d1f
telzhou618 <telzhou618@qq.com>
2020-04-15 00:24:39 +0800

    springboot MyBatis 增删改查

--- springboot-mybatis-crud/src/main/java/com/example/controller/UserCrudController.java
+++ springboot-mybatis-crud/src/main/java/com/example/controller/UserCrudController.java
@@ -1,12 +1,10 @@
 package com.example.controller;
 
-import com.example.entity.User;
+import com.example.common.Result;
 import com.example.mapper.UserMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author zhougaojun
@@ -19,8 +17,8 @@ public class UserCrudController {
     private UserMapper userMapper;
 
     @GetMapping("/user/list")
-    private List<User> userList() {
-        return userMapper.selectAll();
+    private Result userList() {
+        return Result.success(userMapper.selectAll());
     }
 
 }
