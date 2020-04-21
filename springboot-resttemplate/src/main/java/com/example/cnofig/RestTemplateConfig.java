75d7dbe0e45ecaa219a2ee45e711c45a12a0352f
zhougaojun <zhougaojun@dxy.cn>
2020-04-21 20:03:23 +0800

    springboot-resttemplate 实例

--- springboot-resttemplate/src/main/java/com/example/cnofig/RestTemplateConfig.java
+++ springboot-resttemplate/src/main/java/com/example/cnofig/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package com.example.cnofig;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,9 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    @ConditionalOnMissingBean
-    public RestTemplate restTemplate(){
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate;
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- springboot-resttemplate/src/test/java/com/example/SpringTests.java
+++ springboot-resttemplate/src/test/java/com/example/SpringTests.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author jameszhou
  */
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 class SpringTests {
 
 }
