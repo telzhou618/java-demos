c3ab7987931c6de8db9dd8f24a459d8dae9ef3e7
zhougaojun <zhougaojun@dxy.cn>
2020-04-22 10:43:47 +0800

    commons-lang3 实例

---
+++ commons-lang3/src/test/java/com/demo/StringUtilsTests.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+/**
+ * @author zhougaojun
+ */
+@Slf4j
+public class StringUtilsTests {
+
+    /**
+     * 截取文本，超过用...表示，默认截取长度不能小于3,否则抛出异常。
+     */
+    @Test
+    public void abbreviate() {
+        log.info(StringUtils.abbreviate("截取一段文本测试", 5));
+    }
+}
