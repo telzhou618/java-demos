df5f4ecf823e0081cfdb71e37a3376ebf5e0af3a
telzhou618 <telzhou618@qq.com>
2020-04-25 13:35:37 +0800

    guava 实例

--- guava-demo/src/test/java/com/demo/GuavaTest.java
+++ guava-demo/src/test/java/com/demo/GuavaTest.java
@@ -1,10 +1,8 @@
 package com.demo;
 
-import com.google.common.base.Joiner;
-import com.google.common.base.Preconditions;
-import com.google.common.base.Splitter;
-import com.google.common.base.Stopwatch;
+import com.google.common.base.*;
 import com.google.common.collect.Lists;
+import com.google.common.html.HtmlEscapers;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -58,4 +56,24 @@ public class GuavaTest {
         // [a, b, c]
         log.info(Splitter.on(",").trimResults().splitToList("a,b, c").toString());
     }
+
+    /**
+     * Html 转义处理
+     */
+    @Test
+    public void testHtmlEscape() {
+        String html = "<a href=\"http://www.test.cn\">测试</a>";
+
+        // 输出 &lt;a href=&quot;http://www.test.cn&quot;&gt;测试&lt;/a&gt;
+        log.info(HtmlEscapers.htmlEscaper().escape(html));
+
+        // 输出 &lt;a href=&quot;http://www.test.cn&quot;&gt;测试&lt;/a&gt;
+        log.info(htmlEscape(html,HtmlEscapers.htmlEscaper().asFunction()));
+    }
+
+    private String htmlEscape(String html, Function<String, String> function) {
+        return function.apply(html);
+    }
+
+
 }
