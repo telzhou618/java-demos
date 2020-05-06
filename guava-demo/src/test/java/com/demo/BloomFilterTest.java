2ef398a3ef10d9f0e16941afa7d7d2769a7a6650
telzhou618 <telzhou618@qq.com>
2020-05-06 23:05:25 +0800

    guava 实例

---
+++ guava-demo/src/test/java/com/demo/BloomFilterTest.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+import org.junit.Test;
+
+/** 布隆过滤器
+ * @author jameszhou
+ */
+public class BloomFilterTest {
+
+    // 创建布隆过滤器，元素类型int,容量1000,错误率0.001;
+    private BloomFilter<Integer> bloomFilter = BloomFilter.create(Funnels.integerFunnel(),1000,0.001);
+   // private BloomFilter<String> bloomFilter2 = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8),1000,0.001);
+
+    @Test
+    public void test(){
+        // 添加元素
+        for (int i=0;i<1000;i++){
+            bloomFilter.put(i);
+        }
+        // 判断是否存在
+        System.out.println(bloomFilter.mightContain(11));
+        System.out.println(bloomFilter.mightContain(1002));
+    }
+}
--- guava-demo/src/test/java/com/demo/RateLimiterTest.java
+++ guava-demo/src/test/java/com/demo/RateLimiterTest.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-/**
+/** 令牌桶限流
  * @author zhougaojun
  */
 @Slf4j
