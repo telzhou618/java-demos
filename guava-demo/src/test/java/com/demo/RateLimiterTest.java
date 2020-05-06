7fd932b07e0b20e4a3ddbb96b2cfab4f51929d81
zhougaojun <zhougaojun@dxy.cn>
2020-05-06 17:53:03 +0800

    guava 实例

---
+++ guava-demo/src/test/java/com/demo/RateLimiterTest.java
@@ -0,0 +1,36 @@
+package com.demo;
+
+import com.google.common.util.concurrent.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author zhougaojun
+ */
+@Slf4j
+public class RateLimiterTest {
+
+    private static RateLimiter rateLimiter = RateLimiter.create(1);
+    private static Executor executor = Executors.newFixedThreadPool(5);
+
+    @Test
+    public void test() {
+        for (int x = 0; x < 5; x++) {
+            executor.execute(() -> {
+                log.info(Thread.currentThread().getName() + ":" + rateLimiter.tryAcquire());
+            });
+        }
+    }
+
+    /**
+     * 设置超时时间
+     */
+    @Test
+    public void test2() {
+        log.info(Thread.currentThread().getName() + ":" + rateLimiter.tryAcquire(2, 2, TimeUnit.SECONDS));
+    }
+}
