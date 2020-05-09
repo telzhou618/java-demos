8386138f1430133a2f843d47df3a2c0fe4b809da
telzhou618 <telzhou618@qq.com>
2020-05-09 13:07:52 +0800

    guava - 实例之缓存

--- guava-demo/src/test/java/com/demo/GuavaCacheTest.java
+++ guava-demo/src/test/java/com/demo/GuavaCacheTest.java
@@ -23,22 +23,21 @@ public class GuavaCacheTest {
              * expireAfterAccess 是指缓存在指定时间没有被读写时，将失效。
              * refreshAfterWrite是在指定时间内没有被创建/覆盖，则指定时间过后，再次访问时，会去刷新该缓存，在新值没有到来之前，始终返回旧值
              */
-            .expireAfterAccess(3, TimeUnit.SECONDS) // 缓存时间3秒，3没有被读取失效
+            .expireAfterAccess(2, TimeUnit.SECONDS) // 缓存时间3秒，3没有被读取失效
             .build(new CacheLoader<Integer, String>() {
                 @Override
                 public String load(Integer id) throws Exception {
                     String userName = id + "_UserName";
-                    System.out.println("从数据库获取：" + userName);
+                    System.out.println("模拟从数据库获取：" + userName);
                     return userName;
                 }
             });
 
     @Test
-    public void test() {
+    public void test() throws Exception{
         System.out.println(userNameCache.getUnchecked(1)); // 第一次请求从数据库获取
         System.out.println(userNameCache.getUnchecked(1)); // 第二次请求从缓存中获取
-
-        System.out.println(userNameCache.getUnchecked(2));
-        System.out.println(userNameCache.getUnchecked(2));
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println(userNameCache.getUnchecked(1)); // 三秒后缓存失效，再次从数据库获取
     }
 }
