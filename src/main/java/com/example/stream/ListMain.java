41f405292b57e93f383b0191ccc803fc9b5bd1df
zhougaojun <zhougaojun@dxy.cn>
2023-03-24 09:43:47 +0800

    test

---
+++ src/main/java/com/example/stream/ListMain.java
@@ -0,0 +1,12 @@
+package com.example.stream;
+
+public class ListMain {
+    public static void main(String[] args) {
+
+
+//        System.out.println(Runtime.getRuntime().availableProcessors());
+
+//        Lists.newArrayList(1,2,3,4,5,6)
+//                .stream().parallel().forEach(o -> System.out.println(Thread.currentThread().getName()));
+    }
+}
